77f447771ede106c9334afdc8873ce45a7b38b33
danmainah <danielmainawaweru@gmail.com>
2022-04-01 16:59:29 +0300

    Alter index.js to be able to read blog files and remove 404.html we dont need it

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    let path = url.parse(req.url, true);
+    let filename = "." + path.blog;
+    fs.readFileSync(filename, (err, data) => {
+        if(err){
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.end("404 Not Found");
+        }
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+    });
   }).listen(8080);
